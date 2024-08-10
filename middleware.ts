7d458260f7f461a5d80fde6c393f91f6dc7db517
AviratShambharkar <128232370+AviratShambharkar@users.noreply.github.com>
2024-08-11 00:32:10 +0530

    initial commit

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// Define your public routes
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/api/webhook/clerk",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+]);
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
+});
+
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
+};
